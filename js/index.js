74bbe2a8736dff152385ccd696bee5785a9cad7e
James Brooks <brooksjamesk01@gmail.com>
2022-02-22 15:08:19 -0800

    added chart functionality

--- js/index.js
+++ js/index.js
@@ -10,10 +10,11 @@ let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
 let imgThree = document.getElementById('img-three');
 let resultsButton = document.getElementById('results-button');
-let resultsList = document.getElementById('results-list');
+const ctx = document.getElementById('results-chart').getContext('2d');
+let chartWindow = document.getElementById('results-chart');
 
 // Constructor
-function Product(name, fileExtension = 'jpg'){
+function Product(name, fileExtension = 'jpg') {
   this.name = name;
   this.numViews = 0;
   this.numCLicks = 0;
@@ -46,59 +47,138 @@ new Product('wine-glass');
 // Other Code
 
 function randomIndexNum() {
-  return Math.floor(Math.random()*allProducts.length);
+  return Math.floor(Math.random() * allProducts.length);
 }
 
-function renderProducts(){
-  let imagesArray= [];
-  imagesArray[0] = randomIndexNum();
-  imagesArray[1] = randomIndexNum();
-  imagesArray[2] = randomIndexNum();
-  // console.log(imagesArray);
-  while(imagesArray[1] === imagesArray[0]) {
-    imagesArray[1] = randomIndexNum();
+// function renderProducts() {
+//   let imagesArray = [];
+//   imagesArray[0] = randomIndexNum();
+//   imagesArray[1] = randomIndexNum();
+//   imagesArray[2] = randomIndexNum();
+//   // console.log(imagesArray);
+//   while (imagesArray[1] === imagesArray[0]) {
+//     imagesArray[1] = randomIndexNum();
+//   }
+//   while (imagesArray[2] === imagesArray[1] || imagesArray[2] === imagesArray[0]) {
+//     imagesArray[2] = randomIndexNum();
+//   }
+//   imgOne.src = allProducts[imagesArray[0]].src;
+//   imgOne.alt = allProducts[imagesArray[0]].name;
+//   allProducts[imagesArray[0]].numViews++;
+//   imgTwo.src = allProducts[imagesArray[1]].src;
+//   imgTwo.alt = allProducts[imagesArray[1]].name;
+//   allProducts[imagesArray[1]].numViews++;
+//   imgThree.src = allProducts[imagesArray[2]].src;
+//   imgThree.alt = allProducts[imagesArray[2]].name;
+//   allProducts[imagesArray[2]].numViews++;
+// }
+
+let allItems = [];
+// let counter = 1;
+function renderContainerProducts() {
+  let randomIndexes = [];
+  // console.log('allItems', allItems);
+  while (randomIndexes.length < 3) {
+    let randoNum = randomIndexNum();
+    if (!randomIndexes.includes(randoNum) && !allItems.includes(randoNum)) {
+      randomIndexes.push(randoNum);
+    }
   }
-  while(imagesArray[2] === imagesArray[1] || imagesArray[2] === imagesArray[0]) {
-    imagesArray[2] = randomIndexNum();
-  }
-  imgOne.src = allProducts[imagesArray[0]].src;
-  imgOne.alt = allProducts[imagesArray[0]].name;
-  allProducts[imagesArray[0]].numViews++;
-  imgTwo.src = allProducts[imagesArray[1]].src;
-  imgTwo.alt = allProducts[imagesArray[1]].name;
-  allProducts[imagesArray[1]].numViews++;
-  imgThree.src = allProducts[imagesArray[2]].src;
-  imgThree.alt = allProducts[imagesArray[2]].name;
-  allProducts[imagesArray[2]].numViews++;
+  // allItems.push(randomIndexes);
+  allItems.splice(0, 3, ...randomIndexes);
+  // console.table('randomIndexes', randomIndexes);
+  let itemOne = randomIndexes.pop();
+  let itemTwo = randomIndexes.pop();
+  let itemThree = randomIndexes.pop();
+  // console.log(itemOne, itemTwo, itemThree);
+  imgOne.src = allProducts[itemOne].src;
+  imgOne.alt = allProducts[itemOne].name;
+  allProducts[itemOne].numViews++;
+  imgTwo.src = allProducts[itemTwo].src;
+  imgTwo.alt = allProducts[itemTwo].name;
+  allProducts[itemTwo].numViews++;
+  imgThree.src = allProducts[itemThree].src;
+  imgThree.alt = allProducts[itemThree].name;
+  allProducts[itemThree].numViews++;
 }
 
-renderProducts();
+renderContainerProducts();
 
-function handleClick(event){
+function handleClick(event) {
   clicksAllowed--;
   let productClicked = event.target.alt;
 
-  for (let i = 0; i < allProducts.length; i++){
-    if(productClicked === allProducts[i].name){
+  for (let i = 0; i < allProducts.length; i++) {
+    if (productClicked === allProducts[i].name) {
       allProducts[i].numCLicks++;
     }
   }
-  renderProducts();
+  renderContainerProducts();
   if (clicksAllowed === 0) {
     imageContainer.removeEventListener('click', handleClick);
   }
 }
 
 // eslint-disable-next-line no-unused-vars
-function handleResultsButton(event){
-  if (clicksAllowed === 0){
-    for(let i = 0; i < allProducts.length; i++){
-      let li = document.createElement('li');
-      li.textContent = `${allProducts[i].name} was viewed ${allProducts[i].numViews} times, and was clicked ${allProducts[i].numCLicks} times.`;
-      resultsList.appendChild(li);
-    }
+function handleResultsButton(event) {
+  if (clicksAllowed === 0) {
+    renderResultsChart();
+    chartWindow.style.backgroundColor = '#81A851';
   }
 }
 
 imageContainer.addEventListener('click', handleClick);
 resultsButton.addEventListener('click', handleResultsButton);
+
+
+function renderResultsChart() {
+  let productName = [];
+  let productViews = [];
+  let productClicks = [];
+
+  for(let i = 0; i < allProducts.length; i++){
+    productName.push(allProducts[i].name);
+    productViews.push(allProducts[i].numViews);
+    productClicks.push(allProducts[i].numCLicks);
+  }
+  console.log(`Name = ${productName}, Views = ${productViews}, Clicks = ${productClicks}` );
+
+  let chartObject = {
+    type: 'bar',
+    data: {
+      labels: productName,
+      datasets: [{
+        label: '# of Views',
+        data: productViews,
+        backgroundColor: [
+          'rgb(153,0,0)'
+        ],
+        borderColor: [
+          '#c79222'
+        ],
+        borderWidth: 2
+      },
+      {
+        label: '# of Clicks',
+        data: productClicks,
+        backgroundColor: [
+          '#c79222'
+        ],
+        borderColor: [
+          'rgb(153,0,0)'
+        ],
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  // eslint-disable-next-line
+  const myChart = new Chart(ctx, chartObject);
+}
